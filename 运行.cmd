360936f0713b9ac1e56ded2b779890657321c97c
忘忧北萱草 <wybxc@qq.com>
2022-06-10 13:38:19 +0800

    解决找不到CUDA的问题

--- 运行.cmd
+++ 运行.cmd
@@ -1,3 +1,12 @@
 @echo off
+set PATH=%PATH%;%~dp0\python\Library\bin
 cd .\misaka_writer
+if "%1"=="console" goto console
+
 ..\python\pythonw.exe .\run_app.py
+goto end
+
+:console
+..\python\python.exe .\run_app.py
+
+:end
--- 运行（浏览器）.cmd
+++ 运行（浏览器）.cmd
@@ -1,4 +1,5 @@
 @echo off
+set PATH=%PATH%;%~dp0\python\Library\bin
 cd .\misaka_writer
 ..\python\python.exe -m streamlit run app.py
 pause
